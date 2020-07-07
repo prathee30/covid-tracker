a60475067b24f76facc6fa8bf44a908cf5fd23bd
prathee <pratheekshavenkatachalam@gmail.com>
2020-07-07 13:14:21 +0530

    Updates

---
+++ precache-manifest.9f0552ad345b05868c99d0ab76453550.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924cdce959af4d1c6d2a9c0d0d23b5f0",
+    "url": "/track_COVID-19.git/index.html"
+  },
+  {
+    "revision": "eb154dd1946367604522",
+    "url": "/track_COVID-19.git/static/css/main.9ae777de.chunk.css"
+  },
+  {
+    "revision": "48ef1946d4ac37a785f1",
+    "url": "/track_COVID-19.git/static/js/2.93a13140.chunk.js"
+  },
+  {
+    "revision": "98e7206d2472a9ba0c2c6593c9913f5f",
+    "url": "/track_COVID-19.git/static/js/2.93a13140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb154dd1946367604522",
+    "url": "/track_COVID-19.git/static/js/main.952231af.chunk.js"
+  },
+  {
+    "revision": "d5af97705115edc234ee",
+    "url": "/track_COVID-19.git/static/js/runtime-main.01a3c208.js"
+  },
+  {
+    "revision": "fd59fe9ea4258192839496ed65af7086",
+    "url": "/track_COVID-19.git/static/media/image.fd59fe9e.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/track_COVID-19.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
